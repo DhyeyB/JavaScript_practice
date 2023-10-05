47fc5b22cb44a51a11a5c55c74f17420ba9e0186
DhyeyB <dhyeybhattasana1999@gmail.com>
2023-10-05 18:47:06 +0530

    workflow added

---
+++ post-commit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "I am testing post-commit hooks"
+
+# Path to your Python script
+python_script="/home/bs/dhyey/bs/practice/JavaScript_practice/python/sum-of-two-number.py"
+
+# Run the Python script
+python3 "$python_script"
+
+# Check the exit status
+if [ $? -eq 0 ]; then
+  echo "The Python script ran successfully with exit code 0."
+  exit 0
+else
+  echo "The Python script encountered an error with exit code 1."
+  exit 1
+fi
